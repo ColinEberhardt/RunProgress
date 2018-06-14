e5520bf2d8109ea4f6061f65d9623496852acd73
Joshua Pearce <jpearce@scottlogic.co.uk>
2018-06-14 14:52:46 +0100

    Prepare backend for project work

--- server/runprogress/dataProcessor.py
+++ server/runprogress/dataProcessor.py
@@ -10,22 +10,16 @@ class DataProcessor:
         self.me = self.dataService.get_athlete()
 
     def get_week_report(self):
-
-        thisWeeksActivities = list(self.dataService.get_activities(after=getLastMonday()))
-
-        week = getThisWeek()
-
-        days = []
-
-        for d in week:
-            days.append({
-                "day": d.strftime("%A"),
-                "distance": sum(a for a in thisWeeksActivities if a["type"] == "Run" and a["start_date"].date() == d.date()) if d.date() <= datetime.date.today() else None
-            })
-
-        data = {
-            "weeklyRuns": days,
-            "dailyAverage": sum(i["distance"] for i in days if i["distance"] != None) / sum(1 for i in days if i["distance"] != None)
-        }
         
-        return data
+        return {
+            "weeklyRuns": [
+                { "day": "Monday", "distance": 1 },
+                { "day": "Tuesday", "distance": 1 },
+                { "day": "Wednesday", "distance": 1 },
+                { "day": "Thursday", "distance": 1 },
+                { "day": "Friday", "distance": 1 },
+                { "day": "Saturday", "distance": 1 },
+                { "day": "Sunday", "distance": 1 },
+            ],
+            "dailyAverage": 1
+        }
